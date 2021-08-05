8ac0cded3dcf0c1bda9c6ce1dad7d2250d766a8a
aleze89 <68794504+aleze89@users.noreply.github.com>
2021-08-05 15:01:51 -0300

    problemas con usesqlserver solucionado, faltaba using

--- MiAPIDeAlkemy/Startup.cs
+++ MiAPIDeAlkemy/Startup.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MiAPIDeAlkemy
 {
@@ -37,7 +38,7 @@ namespace MiAPIDeAlkemy
             services.AddDbContextPool<MiAPIContext>((services, options) =>
            {
                options.UseInternalServiceProvider(services);
-               options.use
+               options.UseSqlServer();
            });
         }
 
